859f4f54c04c1186a0ff02583e15f252f66b5fd5
spratay <spratay@linuxmail.com>
2022-07-04 15:44:49 +0300

    clear code

--- uprazh6.py
+++ uprazh6.py
@@ -2,4 +2,3 @@ suvenir = input('ведите количество сувениров')
 bezdeluh = input('ведите количество безделух')
 if bezdeluh.isdigit() and suvenir.isdigit():
     print((75 * int(suvenir)) + (112 * int(bezdeluh)))
-exit()
