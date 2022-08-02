f59b234f343710a5e9063cbe1acde3f1f9c14b7d
spratay <spratay@linuxmail.com>
2022-08-02 21:50:00 +0300

    i dont undrestund

--- task2/task8.py
+++ task2/task8.py
@@ -1,8 +1,11 @@
 firs_poss = 10.95
 next_poss = 2.95
 poss = input('Ведите сколько посылок')
-if poss.isdigit() and int(poss) == 1:
+if poss.isdigit() and int(poss) < 0: #непонял вот сейчас не понял
+    print(f'Посылка не может быть {poss}')
+    print(int(f'-{poss}'))
+elif int(poss) == 1:
     print(firs_poss)
 else:
     print(firs_poss + (next_poss * (int(poss) - 1)))
-print(10.95 + 2.95 * 2)
+#print(10.95 + 2.95 * 2)
